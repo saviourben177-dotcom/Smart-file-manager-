e3fe902207f9ac1491045340e4bd027687a96274
saviourben177-dotcom <saviourben177@gmail.com>
2026-06-28 12:32:38 +0100

    Add material icons extended dependency for bottom nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("com.tom-roush:pdfbox-android:2.0.27.0")
